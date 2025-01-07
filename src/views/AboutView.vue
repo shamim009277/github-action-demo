22d9b5856c72c83b541ce1921523d10ab1469ee3
Towhedul <towhidshamim009@gmail.com>
2025-01-07 12:02:17 +0600

    paragraph

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>About Page description</h1>
-    <h1>About Page description</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nesciunt aliquam libero ducimus officia a error, ipsa veritatis fuga beatae?</p>
   </div>
 </template>
 
