78463c29699b02184b60c85eea8c9f54712b10c0
Towhedul <towhidshamim009@gmail.com>
2025-01-07 12:09:12 +0600

    change configration

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/github-action-demo/',  // এখানে 'repository-name' আপনার GitHub রিপোজিটরির নাম হবে
+  build: {
+    outDir: 'dist',  // ভিউ প্রোজেক্টের বিল্ড ফোল্ডার
+  },
   plugins: [
     vue(),
     vueDevTools(),
